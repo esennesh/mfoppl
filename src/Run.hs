82b25cc5c9577941ac280a3b72f00fa587bba893
Eli Sennesh <sennesh.e@northeastern.edu>
2023-07-29 14:08:09 -0400

    src/Run: interpretor for generative PPL effects as reader/writer
    Signed-off-by: Eli Sennesh <sennesh.e@northeastern.edu>

--- src/Run.hs
+++ src/Run.hs
@@ -1,8 +1,36 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeOperators #-}
 module Run (run) where
 
+import Control.Monad.Freer hiding (run)
+import Control.Monad.Freer.Fresh
+import Control.Monad.Freer.Reader as FReader
+import Control.Monad.Freer.Writer
+import qualified Data.Map.Lazy as Map
 import Import
+
+borelDist :: StandardBorel t => Distribution t -> Distribution Borel
+borelDist d = Distribution borelCdf borelPdf (borel . (quantile d)) where
+  borelCdf :: Borel -> Double
+  borelCdf b = fromMaybe 0 $ (cdf d) <$> deBorel b
+  borelPdf :: Borel -> Double
+  borelPdf b = fromMaybe 0 $ (pdf d) <$> deBorel b
+
+runGenerative :: Members '[FReader.Reader HilbertCube, Writer Trace, Fresh] eff
+                 => Eff (Generative ': eff) a -> Eff eff a
+runGenerative = interpret (\case
+  Sample a d -> do
+    cube <- FReader.ask
+    f <- fresh
+    let val = (quantile d) $ cube (a, f) in do
+      tell (Trace (Map.singleton (a, f) (borel val), []))
+      return val
+  Factor w -> tell (Trace (Map.empty, [w])))
 
 run :: RIO App ()
 run = do
