19f82126f9c339bd7b0ea8c15cf9f6e5b196b51b
Eli Sennesh <sennesh.e@northeastern.edu>
2023-07-29 14:06:10 -0400

    src/Types: type defs for standard Borel spaces and PPL effects
    Signed-off-by: Eli Sennesh <sennesh.e@northeastern.edu>

--- src/Types.hs
+++ src/Types.hs
@@ -1,9 +1,21 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GADTs #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE TemplateHaskell #-}
 module Types
-  ( App (..)
+  ( Address
+  , App (..)
+  , Borel (..)
+  , Distribution (..)
+  , Generative (..)
+  , HilbertCube
   , Options (..)
+  , StandardBorel (..)
+  , Trace (..)
   ) where
 
+import Control.Monad.Freer.TH
+import qualified Data.Map.Lazy as Map
 import RIO
 import RIO.Process
 
@@ -23,3 +35,49 @@ instance HasLogFunc App where
   logFuncL = lens appLogFunc (\x y -> x { appLogFunc = y })
 instance HasProcessContext App where
   processContextL = lens appProcessContext (\x y -> x { appProcessContext = y })
+
+data Borel = Unit | Integer Int | Real Double | Product Borel Borel
+             deriving (Eq, Show)
+class StandardBorel t where
+  borel :: t -> Borel
+  deBorel :: Borel -> Maybe t
+instance StandardBorel () where
+  borel () = Unit
+  deBorel Unit = Just ()
+  deBorel _    = Nothing
+instance StandardBorel Int where
+  borel = Integer
+  deBorel (Integer i) = Just i
+  deBorel _           = Nothing
+instance StandardBorel Double where
+  borel = Real
+  deBorel (Real r) = Just r
+  deBorel _        = Nothing
+instance (StandardBorel a, StandardBorel b) => StandardBorel (a, b) where
+  borel (a, b) = Product (borel a) (borel b)
+  deBorel (Product a b) = do
+    ta <- deBorel a
+    tb <- deBorel b
+    return (ta, tb)
+  deBorel _             = Nothing
+
+data Distribution t = Distribution
+  { cdf :: t -> Double
+  , pdf :: t -> Double
+  , quantile :: Double -> t
+  }
+
+data Generative r where
+  Sample :: StandardBorel t => String -> Distribution t -> Generative t
+  Factor :: Double -> Generative ()
+makeEffect ''Generative
+
+type Address = (String, Int)
+type HilbertCube = Address -> Double
+newtype Trace = Trace (Map.Map Address Borel, [Double]) deriving (Eq, Show)
+
+instance Semigroup Trace where
+  (Trace (da, la)) <> (Trace (db, lb)) = Trace (Map.union da db, la ++ lb)
+
+instance Monoid Trace where
+  mempty = Trace (Map.empty, [])
